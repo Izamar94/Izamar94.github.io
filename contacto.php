c63b69f8c01a5597b7c0cd4ef4bed44216e59fd2
Izamar94 <cnm_cruz@hotmail.com>
2020-12-10 08:27:22 -0600

    creacion de vistas

---
+++ contacto.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacto</title>
+    <link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<header class="container-fluid">                    
+    <div class="cont row">
+        <div class="col-sm-2">
+          <img src="resource/logo.png" width="70px" alt="">
+        </div>
+        <div class="col-sm-8">
+            <h2 class="p-3" >Garage Golf VolksWagen</h2>
+        </div>
+        <div class="col-sm-2">
+        </div>
+    </div>
+    <nav class="navbar navbar-expand-sm navbar-light nav">
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false">
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse text-center" id="navbarNav">
+            <ul class="navbar-nav">
+            <li class="nav-item active">
+                    <a href="index.php" class="nav-link" >
+                        Inicio
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a href="mision.php" class="nav-link" >
+                        Misión
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a href="" class="nav-link">
+                        Visión
+                    </a>
+                </li>
+                <li class="nav-item ">
+                  <a href="acercade.php" class="nav-link">
+                      Acerca de
+                  </a>
+                </li>
+                <li class="nav-item ">
+                    <a href="" class="nav-link">
+                        Contacto
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+<body>
+    <div class="container contenido">
+        <div class="content ">    
+            <div class="centrado">
+                <h1>CONTACTO</h1>
+            </div>
+            <hr>
+        </div>
+    </div><br>
+    <div class="container-fluid">
+        <nav aria-label="Page navigation example">
+            <ul class="pagination justify-content-end">
+              <!-- <li class="page-item">
+                <a class="page-link" href="index.html" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                  <span class="sr-only">Previous</span>
+                </a>
+              </li> -->
+              <li class="page-item disabled"><a class="page-link">1</a></li>
+              <li class="page-item "><a class="page-link" href="interes1.html">2</a></li>
+              <li class="page-item"><a class="page-link" href="interes2.html">3</a></li>
+              <li class="page-item"><a class="page-link" href="aboutMe.html">4</a></li>
+              <li class="page-item">
+                <a class="page-link" href="aboutMe.html" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/86d6e3a754.js"></script>
+    </body><br>
+    <footer class="container-fluid">
+        <div class="contenedor row">
+            <div class=" col-sm-5">
+                <p>Mejía Cruz Kristal Izamar</p>
+            </div>
+        </div>
+    </footer>
+</html>
